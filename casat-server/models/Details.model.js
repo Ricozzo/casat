49afdc492afd725a8674736dcfdc415a352239b1
Ricozzo <henriqueortizpereira@gmail.com>
2023-06-06 12:19:53 +0100

    change folder structure

--- casat-server/models/Details.model.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const detailSchema = new Schema({
-    title: String, 
-    description: String,
-    events: {type: Schema.Types.ObjectId, ref:'Event'}
-})
-
-module.exports = model('Detail', detailSchema);
